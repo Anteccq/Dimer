902172c4ff0cc4955ba9e9ab20d3cbd8008da636
AntiqueR <antique.pendulum@gmail.com>
2021-08-01 22:49:06 +0900

    add: !timer Command

---
+++ Dimer/Models/TimerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dimer.Models
+{
+    public class TimerManager
+    {
+        private ConcurrentDictionary<int, MessageTimer> Timers { get; } = new();
+
+        public int Add(MessageTimer timer)
+        {
+            if (timer is null) throw new ArgumentException($"timer can not be null.");
+            var id = Timers.Count;
+            Timers.TryAdd(id, timer);
+            timer.Finished += () => Timers.TryRemove(id, out _);
+            return id;
+        }
+
+        public MessageTimer Find(int key)
+        {
+            Timers.TryGetValue(key, out var timer);
+            return timer;
+        }
+    }
+}
---
+++ Dimer/Modules/CommandModule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dimer.Models;
+using Discord.Commands;
+
+namespace Dimer.Modules
+{
+    public class CommandModule : ModuleBase
+    {
+        private const string TimerInvalidMessage = ":x: `!timer 180 [message]`";
+        [Command("timer")]
+        public async Task Timer(params string[] args)
+        {
+            if (args is null || args.Length < 1)
+            {
+                await ReplyAsync(TimerInvalidMessage);
+                return;
+            }
+
+            if (!TimeParser.TryParse(args[0], out var time))
+            {
+                await ReplyAsync(TimerInvalidMessage);
+                return;
+            }
+
+            await ReplyAsync(time.ToString());
+
+            var message = "";
+            if(args.Length > 1)
+                message = args.Skip(1).Aggregate((a, b) => $"{a} {b}");
+
+            var timer = new MessageTimer(time)
+            {
+                Message = message
+            };
+            timer.Start(async x => await ReplyAsync($"{Context.User.Mention} {x}"));
+        }
+    }
+}
